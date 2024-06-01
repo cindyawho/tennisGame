fc5ee67cf8d04877b2949d20a7a7b8ce8641245c
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-31 22:38:12 -0700

    Create squares to practice coord's

--- game.js
+++ game.js
@@ -9,5 +9,9 @@ window.onload = function() {
     // console.log(canvasContext);
     canvasContext.fillStyle = "black";
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    canvasContext.fillStyle = "red";
+    canvasContext.fillRect(200, 200, 50, 25);
+    canvasContext.fillStyle = "white";
+    canvasContext.fillRect(canvas.width/2-100, canvas.height/2-100, 200, 200);    
     // console.log("The end of function.");
 }
