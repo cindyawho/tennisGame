b248796baec934b93d68fdbd7a12b089e2908f05
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-31 23:11:32 -0700

    Create setInterval: smooth motion

--- game.js
+++ game.js
@@ -1,20 +1,21 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
-var ballY = 50;
+var ballY = 100;
 
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-    drawEverything();
+    setInterval(drawEverything, 30);
 }
 
 function drawEverything() {
-    ballX += 10;
+    ballX += 3;
+    ballY += 3;
     canvasContext.fillStyle = "black";
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
     canvasContext.fillStyle = "red";
-    canvasContext.fillRect(ballX, ballY, 50, 25);
+    canvasContext.fillRect(ballX, ballY, 10, 10);
     canvasContext.fillStyle = "white";
     canvasContext.fillRect(canvas.width/2-100, canvas.height/2-100, 200, 200);
 }
