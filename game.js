77288f1453a1ef4ea5510d5bf9a71f764a5c33dc
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-06-01 11:29:41 -0700

    Create Computer Paddle & logic

--- game.js
+++ game.js
@@ -6,7 +6,9 @@ var ballY = 100;
 var ballSpeedY = 3;
 
 var paddle1Y = 250;
+var paddle2Y = 250;
 const PADDLE_HEIGHT = 100;
+const PADDLE_THICKNESS = 10;
 
 function calculateMousePos(evt) {
     var rect = canvas.getBoundingClientRect();
@@ -46,7 +48,11 @@ function moveEverything(){
     ballY += ballSpeedY;
 
     if(ballX > canvas.width) {
-        ballSpeedX = -ballSpeedX;
+        if(ballY > paddle2Y && ballY < paddle2Y + PADDLE_HEIGHT){
+            ballSpeedX = -ballSpeedX;
+        } else {
+            ballReset();
+        }
     } else if(ballX < 0) {
         if(ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT){
             ballSpeedX = -ballSpeedX;
@@ -67,7 +73,10 @@ function drawEverything() {
     colorRect(0, 0, canvas.width, canvas.height, "black");
 
     //this is the left player paddle
-    colorRect(0, paddle1Y, 10, PADDLE_HEIGHT, "white");
+    colorRect(0, paddle1Y, PADDLE_THICKNESS, PADDLE_HEIGHT, "white");
+
+    //this is the RIGHT COMPUTER paddle
+    colorRect(canvas.width - PADDLE_THICKNESS, paddle2Y, PADDLE_THICKNESS, PADDLE_HEIGHT, "white");
 
     //next line draw the ball
     colorCircle(ballX, ballY, 10, "white")
