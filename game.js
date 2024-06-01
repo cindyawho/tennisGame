2dd53e80e1f13e6ba2d778f273b3d1f95037e538
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-31 22:32:40 -0700

    Create blank canvas

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-console.log("Inside js file, hello world!");
+var canvas;
+var canvasContext;
+
+window.onload = function() {
+    // console.log("Inside js file, hello world!");
+    canvas = document.getElementById('gameCanvas');
+    // console.log(canvas);
+    canvasContext = canvas.getContext('2d');
+    // console.log(canvasContext);
+    canvasContext.fillStyle = "black";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    // console.log("The end of function.");
+}
