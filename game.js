bed26409eb21a1f231dae8e8953f9df2f0b6e3ff
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-31 23:02:49 -0700

    Create function to handle drawings

--- game.js
+++ game.js
@@ -2,16 +2,16 @@ var canvas;
 var canvasContext;
 
 window.onload = function() {
-    // console.log("Inside js file, hello world!");
     canvas = document.getElementById('gameCanvas');
-    // console.log(canvas);
     canvasContext = canvas.getContext('2d');
-    // console.log(canvasContext);
+    drawEverything();
+}
+
+function drawEverything() {
     canvasContext.fillStyle = "black";
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
     canvasContext.fillStyle = "red";
     canvasContext.fillRect(200, 200, 50, 25);
     canvasContext.fillStyle = "white";
-    canvasContext.fillRect(canvas.width/2-100, canvas.height/2-100, 200, 200);    
-    // console.log("The end of function.");
+    canvasContext.fillRect(canvas.width/2-100, canvas.height/2-100, 200, 200);
 }
